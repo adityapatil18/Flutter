8c009f58dd8a716bfffad1b9bcc90f96836f020e
adityapatil18 <96226511+adityapatil18@users.noreply.github.com>
2023-02-24 18:54:10 +0530

    checkbox
    A checkbox is a type of input component which holds the Boolean value. It is a GUI element that allows the user to choose multiple options from several selections. We can use following properties
    1.Value:It is used whether the checkbox is checked or not.
    2.OnChanged: It will be called when the value is changed.
    3.ListStyle: It provides label to checkbox. its properties are title and leading.

---
+++ Flutter basic programs/checkbox.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/app_screen/screen6.dart';
+//checkbox
+
+class MyWidget7 extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return (MyState());
+  }
+}
+
+class MyState extends State<MyWidget7> {
+  bool? flag1 = true;
+  bool? flag2 = true;
+  String msg = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+      home: Scaffold(
+          body: Column(
+            children: [
+              ListTile(
+                title: Text('Dart'),
+                leading: Checkbox(
+                  value: flag1,
+                  onChanged: (value) {
+                    setState(() {
+                      flag1 = value;
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                title: Text('C'),
+                leading: Checkbox(
+                  value: flag2,
+                  onChanged: (value) {
+                    setState(() {
+                      flag2 = value;
+                    });
+                  },
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: (() {
+                    setState(() {
+                      msg = "";
+                      if (flag1 == true) {
+                        msg = "Dart";
+                      }
+                      if (flag2 == true) {
+                        msg += "C";
+                      }
+                    });
+                  }),
+                  child: Text('ok')),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Text('you selected  $msg '),
+              )
+            ],
+          ),
+          appBar: AppBar(
+            title: Text('textfiled'),
+          )),
+    ));
+  }
+}
